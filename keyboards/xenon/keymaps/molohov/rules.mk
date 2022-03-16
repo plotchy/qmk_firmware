1724210e1e45ffd48bfc58d594dcdee007f43a2e
plotchy <plotchy@tutanota.com>
2022-03-15 22:57:01 -0400

    init

---
+++ keyboards/xenon/keymaps/molohov/rules.mk
@@ -0,0 +1,13 @@
+OLED_ENABLE = yes
+RGBLIGHT_ENABLE = no
+EXTRAKEY_ENABLE = yes
+LTO_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+
+# Disable unsupported hardware, maybe it reduces firmware size?
+AUDIO_SUPPORTED = no
+BACKLIGHT_SUPPORTED = no
+CONSOLE_ENABLE = no
+MOUSEKEY_ENABLE = no
+VIA_ENABLE = no
---
+++ users/molohov/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// For home row mods
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+//#define HRM
+// #define IGNORE_MOD_TAP_INTERRUPT
+
+#define ADAPTIVE_TERM 300
+
+#define COMBO_STRICT_TIMER
+#define COMBO_TERM 20
+#define NO_ACTION_ONESHOT
+// #define SPLIT_MODS_ENABLE
+#define SPLIT_LAYER_STATE_ENABLE
